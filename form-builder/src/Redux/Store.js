22647eb809f499320a77e3ac662d05dc68843218
Shyam Mahajan <smahajan@hgsdigital.com>
2021-11-16 23:15:48 +0530

    removed react dev tools

--- form-builder/src/Redux/Store.js
+++ form-builder/src/Redux/Store.js
@@ -10,9 +10,9 @@ const configureStore = () => {
   const store = createStore(
     rootReducer,
     compose(
-      applyMiddleware(sagaMiddleware, logger),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+      applyMiddleware(sagaMiddleware, logger)
+      // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      //   window.__REDUX_DEVTOOLS_EXTENSION__()
     )
   );
   sagaMiddleware.run(rootSaga)
